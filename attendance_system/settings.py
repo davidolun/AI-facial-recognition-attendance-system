957d3b58d76d100c368f356aefcb3ab8b9e97f76
wavydips <wavydips@gmail.com>
2025-11-11 21:39:48 -0800

    made changes to the API keys that was leaked

--- attendance_system/settings.py
+++ attendance_system/settings.py
@@ -23,7 +23,7 @@ except ImportError:
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+#fixed leaks
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
